f562b22782080941266f939957ef449ad7f62caa
bmac228p <60240732+bmac228p@users.noreply.github.com>
2022-03-07 12:56:14 -0800

    Add files via upload
    Database upload from LMU Build MyPHP Admin

---
+++ localhost.sql
@@ -0,0 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 07, 2022 at 08:54 PM
+-- Server version: 5.7.37
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bmackell_baseballanalytics2020`
+--
+CREATE DATABASE IF NOT EXISTS `bmackell_baseballanalytics2020` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bmackell_baseballanalytics2020`;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
